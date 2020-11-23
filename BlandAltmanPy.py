e34b37caf9402d1d613d0db711a8fbe4e9b6bb1b
Matt <mpatterson@shimmersensing.com>
2020-11-23 09:04:31 -0500

    fix error in RMSE calc

--- BlandAltmanPy.py
+++ BlandAltmanPy.py
@@ -32,7 +32,7 @@ class BlandAltman():
         self.mean_error = diffs.mean()
         self.mean_absolute_error = diffs.abs().mean()
         self.mean_squared_error = (diffs ** 2).mean()
-        self.root_mean_squared_error = np.sqrt(diffs**2).mean()
+        self.root_mean_squared_error = np.sqrt((diffs**2).mean())
         r = np.corrcoef(self.gold_std,self.new_measure)
         self.correlation = r[0,1]  # correlation coefficient
         diffs_std = diffs.std()    # 95% Confidence Intervals
